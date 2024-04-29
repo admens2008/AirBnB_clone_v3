1f113836b302b158692d88addfee90f739813bdb
admens2008 <adjeimensah@gmail.com>
2024-04-29 09:06:50 +0100

    states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -64,7 +64,7 @@ def put_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.get_json():
+    if not request.is_json or not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for attr, val in request.get_json().items():
         if attr not in ['id', 'created_at', 'updated_at']:
